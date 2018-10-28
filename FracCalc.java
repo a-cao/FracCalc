429d00efb1e40cbc96ed99be5722669a1bf5a26e
a-cao <41025779+a-cao@users.noreply.github.com>
2018-10-28 14:30:00 -0700

    Add files via upload
    Checkpoint 1 - Anna Cao

---
+++ FracCalc.java
@@ -0,0 +1,36 @@
+package fracCalc;
+
+import java.util.*;
+public class FracCalc {
+
+    public static void main(String[] args) 
+    {
+        // TODO: Read the input from the user and call produceAnswer with an equation
+    	Scanner input = new Scanner(System.in);
+    	System.out.println("Input an equation: ");
+    	String equation = input.next();
+    	produceAnswer(equation);
+
+    }
+    
+    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
+    // This function takes a String 'input' and produces the result
+    //
+    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
+    //      e.g. input ==> "1/2 + 3/4"
+    //        
+    // The function should return the result of the fraction after it has been calculated
+    //      e.g. return ==> "1_1/4"
+    public static String produceAnswer(String input)
+    { 
+        // TODO: Implement this function to produce the solution to the input
+        String operator = input.substring((input.indexOf(" ") + 1),(input.indexOf(" ") + 2));
+        String fraction1 = input.substring(0, input.indexOf(" "));
+        String fraction2 = input.substring(input.indexOf(" ") + 3);
+
+        return fraction2;
+    }
+
+    // TODO: Fill in the space below with any helper methods that you think you will need
+    
+}
